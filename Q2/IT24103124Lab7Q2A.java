87b871f65ed105b6f77d4c9473f81c1c002b6418
IT24103124 <IT24103124@my.sliit.lk>
2024-10-03 23:30:08 +0530

    IT24103124Lab7Q2A

---
+++ Q2/IT24103124Lab7Q2A.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+	public class IT24103124Lab7Q2A {
+		public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int i , j ;
+		for ( i = 1; i<=5; i = i+1) {
+			for (j=1; j<=5; j=j+1) {
+				System.out.print("$"+ " ");
+
+			}
+			System.out.println(" ");	
+		}
+	}
+}
