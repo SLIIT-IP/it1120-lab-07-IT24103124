0d34a4309d404ad852d1ad0c62f33500559978da
IT24103124 <IT24103124@my.sliit.lk>
2024-10-04 00:14:54 +0530

    IT24103124Lab7Q2C

---
+++ Q2/IT24103124Lab7Q2C.java
@@ -0,0 +1,16 @@
+ public class IT24103124Lab7Q2C {
+		public static void main(String[] args) {
+		
+		int i , j ;
+		
+		for ( i = 5; i >=1; i--)
+		{
+			
+			for (j=1; j<=i; j=j+1)
+		{
+			System.out.print( i );
+		}
+			System.out.println();
+			}
+	}
+}
